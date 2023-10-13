f1ce78fd826ea80ca21d896761dc939f4eb1dda3
Zhenya Osokina <kapitan.kub@gmail.com>
2023-10-13 14:33:32 +0500

    Реализация класса Init и тесты к нему

---
+++ homework-1/tests/test_item.py
@@ -0,0 +1,15 @@
+from src.item import Item
+
+
+def test_apply_discount():
+    item3 = Item('Смарт-часы', 5000, 5)
+    item3.apply_discount()
+    assert 4000 * Item.pay_rate == 3400
+
+
+def test_calculate_total_price():
+    item3 = Item('Смарт-часы', 5000, 5)
+    assert item3.calculate_total_price() == 25000
+
+
+#
