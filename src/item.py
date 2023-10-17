78465d50dc3b9e6bcdccc6d2497d522d327ee2e1
Zhenya Osokina <kapitan.kub@gmail.com>
2023-10-18 01:32:36 +0500

    Start hw2

--- src/item.py
+++ src/item.py
@@ -1,8 +1,12 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
-    pay_rate = 0,85
+    pay_rate = 0.85
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
@@ -13,11 +17,47 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         Item.all.append(self)
+
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name_str):
+        self.__name = name_str[:10]
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """instantiate_from_csv() - класс-метод, инициализирующий
+        экземпляры класса Item данными из файла src/items.csv"""
+        cls.all.clear()
+        items_csv_read = os.path.join('../src/items.csv')
+        with open(items_csv_read, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            cls.all = []
+            for row in reader:
+                item = cls(row['name'], row['price'], row['quantity'])
+                cls.all.append(item)
+                # name = row['name']
+                # price = float(row['price'])
+                # quantity = int(row['quantity'])
+                # item = cls(name, price, quantity)
+                # items.append(item)
+
+
+
+    @staticmethod
+    def string_to_number(num):
+        """статический метод, возвращающий число из числа-строки"""
+        return int(float(num))
+
 
     def calculate_total_price(self) -> float:
         """
@@ -34,5 +74,5 @@ class Item:
         self.price *= self.pay_rate
 
 
-    def __repr__(self):
-        return f"{self.name}, {self.price}, {self.quantity}"
+    # def __repr__(self):
+    #     return f"{self.__name}, {self.price}, {self.quantity}"
