7713bb84361a5c06ef336bb9295ba40b53ae98bf
Zhenya Osokina <kapitan.kub@gmail.com>
2023-10-18 14:02:17 +0500

    Ошибки исключила, main.py запускается

--- src/item.py
+++ src/item.py
@@ -17,52 +17,44 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.__name = name
+        self.__name = name  # атрибут name сделать приватным
         self.price = price
         self.quantity = quantity
-
         Item.all.append(self)
-
 
     @property
     def name(self):
+        """добавлен геттер для name, используя @property"""
+
         return self.__name
 
     @name.setter
     def name(self, name_str):
+        """добавлен сеттер для name, используя @property"""
         self.__name = name_str[:10]
 
-
     @classmethod
-    def instantiate_from_csv(cls):
+    def instantiate_from_csv(cls, file_path):
         """instantiate_from_csv() - класс-метод, инициализирующий
         экземпляры класса Item данными из файла src/items.csv"""
-        cls.all.clear()
-        items_csv_read = os.path.join('../src/items.csv')
-        with open(items_csv_read, newline='') as csvfile:
+
+        # items_csv_read = os.path.join('../src/items.csv')
+        with open(file_path, mode='r', encoding='windows-1251') as csvfile:
             reader = csv.DictReader(csvfile)
-            cls.all = []
+            cls.all.clear()
             for row in reader:
-                item = cls(row['name'], row['price'], row['quantity'])
-                cls.all.append(item)
-                # name = row['name']
-                # price = float(row['price'])
-                # quantity = int(row['quantity'])
-                # item = cls(name, price, quantity)
-                # items.append(item)
-
-
+                item = cls(row['name'], float(row['price']), int(row['quantity']))
+                if item not in cls.all:
+                    cls.all.append(item)
 
     @staticmethod
     def string_to_number(num):
         """статический метод, возвращающий число из числа-строки"""
         return int(float(num))
 
-
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
         return self.price * self.quantity
@@ -73,6 +65,5 @@ class Item:
         """
         self.price *= self.pay_rate
 
-
-    # def __repr__(self):
-    #     return f"{self.__name}, {self.price}, {self.quantity}"
+    def __repr__(self):
+        return f"{self.__name}, {self.price}, {self.quantity}"
